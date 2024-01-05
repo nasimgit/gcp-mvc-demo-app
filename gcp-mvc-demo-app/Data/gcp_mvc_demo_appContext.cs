8ea30ed44bec91a5db88461edc58fa1022c4e6b5
Md. Nazmul Hasan Nasim <iamnazmulhasan@gmail.com>
2024-01-05 15:41:15 +0600

    initial

---
+++ gcp-mvc-demo-app/Data/gcp_mvc_demo_appContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using gcp_mvc_demo_app.Models;
+
+namespace gcp_mvc_demo_app.Data
+{
+    public class gcp_mvc_demo_appContext : DbContext
+    {
+        public gcp_mvc_demo_appContext (DbContextOptions<gcp_mvc_demo_appContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<gcp_mvc_demo_app.Models.Movie> Movie { get; set; } = default!;
+    }
+}
