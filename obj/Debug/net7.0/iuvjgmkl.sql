987fd71ee1f1d141befcf094d6b8e3441938f006
Md. Nazmul Hasan Nasim <iamnazmulhasan@gmail.com>
2024-01-05 23:15:11 +0600

    connection string updated

---
+++ obj/Debug/net7.0/iuvjgmkl.sql
@@ -0,0 +1,49 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20240105092257_InitialCreate')
+BEGIN
+    CREATE TABLE [Movie] (
+        [Id] int NOT NULL IDENTITY,
+        [Title] nvarchar(max) NULL,
+        [ReleaseDate] datetime2 NOT NULL,
+        [Genre] nvarchar(max) NULL,
+        [Price] decimal(18,2) NOT NULL,
+        CONSTRAINT [PK_Movie] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20240105092257_InitialCreate')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20240105092257_InitialCreate', N'7.0.12');
+END;
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20240105112756_initial-miration')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20240105112756_initial-miration', N'7.0.12');
+END;
+GO
+
+COMMIT;
+GO
+
