f4d5d9e5f64ca6e162dfb87f910182b16a7f27fb
Md. Nazmul Hasan Nasim <iamnazmulhasan@gmail.com>
2024-01-05 16:45:07 +0600

    cloudbuild file added

--- gcp-mvc-demo-app/obj/Debug/net7.0/gcp-mvc-demo-app.AssemblyInfo.cs
+++ gcp-mvc-demo-app/obj/Debug/net7.0/gcp-mvc-demo-app.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("d7b6034c-7e78-436b-a443-340556d6d992")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("gcp-mvc-demo-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
