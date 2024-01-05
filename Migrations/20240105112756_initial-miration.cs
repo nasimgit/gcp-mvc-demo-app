8357cad2db44bb004ccd5d9649ed9bd9ccb7e414
Md. Nazmul Hasan Nasim <iamnazmulhasan@gmail.com>
2024-01-05 18:42:24 +0600

    latest files

---
+++ Migrations/20240105112756_initial-miration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace gcp_mvc_demo_app.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialmiration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
